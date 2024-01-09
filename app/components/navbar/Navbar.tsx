b7464628d255a5b798e9ca92b25e49e2c787844a
sam <samuel6249ng@gmail.com>
2024-01-10 00:02:29 +1100

    image,icon and navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,40 +4,32 @@ import Image from "next/image"
 
 
 
-interface NavbarProps {
-  children: React.ReactNode;
-}
-
-const Navbar: React.FC<NavbarProps> = ({
-  children
-}) => {
-  console.log({children});
+const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-        <div className="py-4 border-[1px]">
-            <Container>
-                <div className="
-                  flex
-                  flex-row
-                  items-center
-                  justify-between
-                  gap-3
-                  md:gap-0
-                "
-                >
-                  <Image
-            alt="sam"
-            className="hidden md:block cursor-pointer"
-            height="200"
-            width="200"
-            src="/image/sam.jpeg" 
-        />
-                </div>
-            </Container>
+    <div className="py-4 border-[1px]">
+        <Container>
+            <div className="
+              flex
+              flex-row
+              items-center
+              justify-between
+              gap-3
+              md:gap-0
+            "
+            >
+              <Image
+        alt="sam"
+        className="hidden md:block cursor-pointer"
+        height="200"
+        width="200"
+        src="/image/sam.jpeg" 
+    />
             </div>
-            <Categories />
-        </div>  
-        );
+        </Container>
+        </div>
+        <Categories />
+    </div>    )
 }
 
 export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         
-        <Navbar children={undefined} />
+        <Navbar />
         
         
         {children}
