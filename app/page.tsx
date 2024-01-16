5037b1776dc6a4e1dbbd926afcb8cc2d64767ca7
sam <samuel6249ng@gmail.com>
2024-01-16 22:28:43 +1100

    working on components

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Container  from './components/Container'
 import YoutubeEmbed from './components/Video'
 import Video from './components/Video'
-import Blue from './components/blue'
 import Base from './components/Base'
 import Navbar from './components/navbar/Navbar'
 
